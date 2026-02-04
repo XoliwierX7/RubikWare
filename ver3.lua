d81affddafcf581a40096d975e314c64bed89b8a
XoliwierX7 <moveminiendo87@gmail.com>
2026-02-04 13:33:08 +0100

    Disable FriendCheck in ver3.lua

--- ver3.lua
+++ ver3.lua
@@ -8,7 +8,7 @@ local Holding = false
 -- === USTAWIENIA ===
 _G.AimbotEnabled = true
 _G.TeamCheck = false    
-_G.FriendCheck = true   
+_G.FriendCheck = false   
 _G.AimPart = "Head" 
 _G.AutoShoot = true     
 _G.FOVRadius = 150
